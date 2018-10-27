f33d3ee558ec34493af4b14b386655708cc0b1e4
jeanyego <44524100+jeanyego@users.noreply.github.com>
2018-10-27 15:15:24 +0300

    Portfolio
    this is my portfolio website.

---
+++ portfolio/js/scripts.js
@@ -0,0 +1,21 @@
+$(Document).ready(function(){
+    $("button").click(function(){
+    $("img").hide();
+});
+$("button").dblclick(function(){
+    $("img").show();
+});
+});
+var name="Jean"
+var age="19"
+console.log(name+""+age);
+//alert("Fake it till you make it");
+//prompt("what motivates you to workout everyday?");
+function firstFunction(){
+  //document.write(name);
+  document.getElementById("fitness").innerHTML="Work out ";
+}; 
+//firstFunction()
+//function hellofunction(){
+    //alert("hallo,vergessen sie zum taglichen training nicht"); 
+    
